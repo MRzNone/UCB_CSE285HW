84e4a3bf74fff7f2bb2c85cc5096c216a2dd5e83
MRzNone <whzeng98@gmail.com>
2021-07-06 01:30:05 +0800

    fix replay buffer. imitation working

--- hw1/cs285/infrastructure/replay_buffer.py
+++ hw1/cs285/infrastructure/replay_buffer.py
@@ -18,7 +18,7 @@ class ReplayBuffer(object):
         self.terminals = None
 
     def __len__(self):
-        if self.obs:
+        if self.obs is not None:
             return self.obs.shape[0]
         else:
             return 0
@@ -76,7 +76,7 @@ class ReplayBuffer(object):
         ## HINT 1: use np.random.permutation to sample random indices
         ## HINT 2: return corresponding data points from each array (i.e., not different indices from each array)
         ## HINT 3: look at the sample_recent_data function below
-        idx = np.random.choice(len(self.paths), batch_size)
+        idx = np.random.choice(len(self), batch_size)
         obs = self.obs[idx]
         acs = self.acs[idx]
         rews = self.rews[idx]
